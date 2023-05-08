61611cf0f795221615e4e802bf8209280b1ef854
Robin van der Noord <robinvandernoord@gmail.com>
2023-05-08 15:16:39 +0200

    feat: import main tasks to __init__ so this plugin can be used as a library

--- src/edwh_pipcompile_plugin/__init__.py
+++ src/edwh_pipcompile_plugin/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-FileCopyrightText: 2023-present Remco Boerma <remco.b@educationwarehouse.nl>
 #
 # SPDX-License-Identifier: MIT
+
+from .pipcompile_plugin import compile, install, remove, upgrade
